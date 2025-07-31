d97878eaafe82798ab656290af3028659ae2644c
Stefan Musarra <stefan0@stefan0.com>
2025-07-30 17:50:06 -0700

    replace object literal with fetch

--- script.js
+++ script.js
@@ -1,317 +1,55 @@
-const books = [
-  {
-    title: "Frankenstein or The Modern Prometheus",
-    key: "/works/OL450063W",
-    cover_id: 12356249,
-    authors: [
-      {
-        key: "/authors/OL25342A",
-        name: "Mary Shelley",
-      },
-    ],
-    first_publish_year: 1818,
-    edition_count: 2183,
-  },
-  {
-    title: "The Princess and Curdie",
-    key: "/works/OL15448W",
-    cover_id: 14357835,
-    authors: [
-      {
-        key: "/authors/OL23082A",
-        name: "George MacDonald",
-      },
-    ],
-    first_publish_year: 1832,
-    edition_count: 415,
-  },
-  {
-    title: "Alice's Adventures in Wonderland",
-    key: "/works/OL138052W",
-    cover_id: 10527843,
-    authors: [
-      {
-        key: "/authors/OL22098A",
-        name: "Lewis Carroll",
-      },
-    ],
-    first_publish_year: 1865,
-    edition_count: 3546,
-  },
-  {
-    title: "Vingt mille lieues sous les mers",
-    key: "/works/OL1099280W",
-    cover_id: 6573517,
-    authors: [
-      {
-        key: "/authors/OL113611A",
-        name: "Jules Verne",
-      },
-      {
-        key: "/authors/OL2832142A",
-        name: "Serge Micheli",
-      },
-      {
-        key: "/authors/OL2832141A",
-        name: "Francois Riviere",
-      },
-    ],
-    first_publish_year: 1870,
-    edition_count: 382,
-  },
-  {
-    title: "The Strange Case of Dr. Jekyll and Mr. Hyde",
-    key: "/works/OL24156W",
-    cover_id: 295773,
-    authors: [
-      {
-        key: "/authors/OL25963A",
-        name: "Robert Louis Stevenson",
-      },
-    ],
-    first_publish_year: 1875,
-    edition_count: 360,
-  },
-  {
-    title: "Flatland",
-    key: "/works/OL118388W",
-    cover_id: 10069547,
-    authors: [
-      {
-        key: "/authors/OL20585A",
-        name: "Edwin Abbott Abbott",
-      },
-    ],
-    first_publish_year: 1884,
-    edition_count: 543,
-  },
-  {
-    title: "The Time Machine",
-    key: "/works/OL52267W",
-    cover_id: 9009316,
-    authors: [
-      {
-        key: "/authors/OL13066A",
-        name: "H. G. Wells",
-      },
-    ],
-    first_publish_year: 1895,
-    edition_count: 1151,
-  },
-  {
-    title: "The Island of Dr. Moreau",
-    key: "/works/OL381550W",
-    cover_id: 968312,
-    authors: [
-      {
-        key: "/authors/OL13066A",
-        name: "H. G. Wells",
-      },
-      {
-        key: "/authors/OL7563369A",
-        name: "Fiona Beddall",
-      },
-      {
-        key: "/authors/OL2825724A",
-        name: "Dobbs",
-      },
-      {
-        key: "/authors/OL3101873A",
-        name: "Fabrizio Fiorentino",
-      },
-      {
-        key: "/authors/OL7934538A",
-        name: "David Domínguez",
-      },
-      {
-        key: "/authors/OL7708382A",
-        name: "Jonathan Kent",
-      },
-      {
-        key: "/authors/OL52922A",
-        name: "Margaret Atwood",
-      },
-      {
-        key: "/authors/OL12605439A",
-        name: "Judit Lligonya Tenasen",
-      },
-      {
-        key: "/authors/OL12600907A",
-        name: "Manuel Rivas Cabezuelo",
-      },
-      {
-        key: "/authors/OL12605440A",
-        name: "Amaia Gurbindo",
-      },
-      {
-        key: "/authors/OL7969831A",
-        name: "Judit Lligonya Tenas",
-      },
-      {
-        key: "/authors/OL12605446A",
-        name: "Marta Seoane Rebollo",
-      },
-    ],
-    first_publish_year: 1896,
-    edition_count: 273,
-  },
-  {
-    title: "Dracula",
-    key: "/works/OL85892W",
-    cover_id: 12216503,
-    authors: [
-      {
-        key: "/authors/OL31727A",
-        name: "Bram Stoker",
-      },
-    ],
-    first_publish_year: 1897,
-    edition_count: 730,
-  },
-  {
-    title: "The Invisible Man",
-    key: "/works/OL52266W",
-    cover_id: 6419199,
-    authors: [
-      {
-        key: "/authors/OL13066A",
-        name: "H. G. Wells",
-      },
-    ],
-    first_publish_year: 1897,
-    edition_count: 416,
-  },
-  {
-    title: "The War of the Worlds",
-    key: "/works/OL52114W",
-    cover_id: 36314,
-    authors: [
-      {
-        key: "/authors/OL13066A",
-        name: "H. G. Wells",
-      },
-    ],
-    first_publish_year: 1898,
-    edition_count: 429,
-  },
-  {
-    title: "The Wonderful Wizard of Oz",
-    key: "/works/OL18417W",
-    cover_id: 552443,
-    authors: [
-      {
-        key: "/authors/OL23431A",
-        name: "L. Frank Baum",
-      },
-    ],
-    first_publish_year: 1899,
-    edition_count: 2052,
-  },
-  {
-    title: "The Lost World",
-    key: "/works/OL262460W",
-    cover_id: 8231444,
-    authors: [
-      {
-        key: "/authors/OL161167A",
-        name: "Arthur Conan Doyle",
-      },
-    ],
-    first_publish_year: 1900,
-    edition_count: 747,
-  },
-  {
-    title: "The Iron Heel",
-    key: "/works/OL74502W",
-    cover_id: 8243314,
-    authors: [
-      {
-        key: "/authors/OL44633A",
-        name: "Jack London",
-      },
-    ],
-    first_publish_year: 1907,
-    edition_count: 594,
-  },
-  {
-    title: "The Secret Agent",
-    key: "/works/OL39108W",
-    cover_id: 8239401,
-    authors: [
-      {
-        key: "/authors/OL19441A",
-        name: "Joseph Conrad",
-      },
-    ],
-    first_publish_year: 1907,
-    edition_count: 255,
-  },
-  {
-    title: "The Poison Belt",
-    key: "/works/OL262596W",
-    cover_id: 5788361,
-    authors: [
-      {
-        key: "/authors/OL161167A",
-        name: "Arthur Conan Doyle",
-      },
-    ],
-    first_publish_year: 1913,
-    edition_count: 345,
-  },
-  {
-    title: "At The Earth's Core And Out Of Time's Abyss",
-    key: "/works/OL1417907W",
-    cover_id: 9317372,
-    authors: [
-      {
-        key: "/authors/OL146605A",
-        name: "Edgar Rice Burroughs",
-      },
-    ],
-    first_publish_year: 1914,
-    edition_count: 223,
-  },
-  {
-    title: "Brave New World",
-    key: "/works/OL64365W",
-    cover_id: 8231823,
-    authors: [
-      {
-        key: "/authors/OL19767A",
-        name: "Aldous Huxley",
-      },
-    ],
-    first_publish_year: 1932,
-    edition_count: 675,
-  },
-  {
-    title: "Nineteen Eighty-Four",
-    key: "/works/OL1168083W",
-    cover_id: 9267242,
-    authors: [
-      {
-        key: "/authors/OL118077A",
-        name: "George Orwell",
-      },
-    ],
-    first_publish_year: 1949,
-    edition_count: 433,
-  },
-  {
-    title: "Fahrenheit 451",
-    key: "/works/OL103123W",
-    cover_id: 12993656,
-    authors: [
-      {
-        key: "/authors/OL24137A",
-        name: "Ray Bradbury",
-      },
-    ],
-    first_publish_year: 1953,
-    edition_count: 255,
-  },
-];
+const subject = "science_fiction";
+const limit = 20;
+const offset = 0;
+const uri = `https://openlibrary.org/subjects/${subject}.json?limit=${limit}&offset=${offset}`;
+
+const fetchData = async () => {
+  const timeOutMs = 5000;
+  /*
+  The API at openlibrary.org does not support CORS.
+  Request header field content-type is not allowed by Access-Control-Allow-Headers 
+  in preflight response and Request header field cors is not allowed.
+  */
+  const response = await fetch(uri, {
+    method: "GET",
+    cors: "no-cors",
+    // headers: {
+    //   "Content-Type": "application/json",
+    // },
+    timeout: timeOutMs,
+  });
+  if (!response.ok) {
+    alert(`HTTP error! status: ${response.status}`);
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+  const data = await response.json();
+  return data;
+};
+
+const extractWorks = (data) => {
+  const works = data.works.map((work) => {
+    return {
+      title: work.title,
+      key: work.key,
+      cover_id: work.cover_id,
+      authors: work.authors,
+      first_publish_year: work.first_publish_year,
+      edition_count: work.edition_count,
+    };
+  });
+  sortWorksByDate(works);
+  return works;
+};
+
+const sortWorksByDate = (works) =>
+  works.sort((a, b) => {
+    return a.first_publish_year - b.first_publish_year;
+  });
+
+const fetchBooks = async () => {
+  const data = await fetchData();
+  return extractWorks(data);
+};
 
 const main = document.querySelector("main");
 
@@ -345,8 +83,9 @@ function createBookCard(book) {
   return clone;
 }
 
-function render() {
+async function render() {
   const fragment = document.createDocumentFragment();
+  const books = await fetchBooks();
   books.forEach((book) => {
     fragment.appendChild(createBookCard(book));
   });
